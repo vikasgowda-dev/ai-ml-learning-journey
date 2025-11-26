3e011c4d611d301d57e12744f6b62df78e6cc71d
vikasgowda-dev <mnvikasgowda@gmail.com>
2025-11-26 20:23:00 +0530

    Add airthematic operator example

---
+++ 01-python-for-ml/04-operators.py
@@ -0,0 +1,14 @@
+
+# 1) Airthemetic operator?
+#  - Used for mathematical calculations like +,-,*,/,%,**,//.
+
+a = 10
+b = 3 
+
+print("Addition:",a+b)
+print("Substraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Modulus:",a%b)
+print("Exponent:",a**b)
+print("Floor Division:",a//b)
