607e653af0c25441df300a5b2d108af7cc1226e2
vikasgowda-dev <mnvikasgowda@gmail.com>
2025-11-27 13:51:22 +0530

    Add relational operator example

--- 01-python-for-ml/04-operators.py
+++ 01-python-for-ml/04-operators.py
@@ -4,7 +4,7 @@
 
 a = 10
 b = 3 
-
+print("-----------Airthemetic Operator-----------")
 print("Addition:",a+b)
 print("Substraction:",a-b)
 print("Multiplication:",a*b)
@@ -12,3 +12,17 @@ print("Division:",a/b)
 print("Modulus:",a%b)
 print("Exponent:",a**b)
 print("Floor Division:",a//b)
+
+
+# 2) Relational operator?
+# - Compare two values and return true or false.
+
+x = 5 
+y = 10
+print("------Relational Operator-------")
+print("Equal to:",x == y)
+print("Not Equal:",x!=y)
+print("Greater-than:",x>y)
+print("Less-than:",x<y)
+print("Greater or Equal:",x>=y)
+print("Less or Equal:",x<=y)
