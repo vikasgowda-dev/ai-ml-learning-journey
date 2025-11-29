fbef9302215151caa7fb3f65827bd4368b5f4c8f
vikasgowda-dev <mnvikasgowda@gmail.com>
2025-11-29 22:21:55 +0530

    Add operator precedence concept with examples

---
+++ 01-python-for-ml/05-operator_precedence.py
@@ -0,0 +1,35 @@
+# Operator precedence 
+# - Operator precedence decides which operations are done first in a Python expression.
+
+
+
+#                                Higer -----------------> Lower
+# (),**,'+x''-x''~x',*///%,+-,'<<''>>',&,^,|,'==''!='>'<'>='<=',not,or,'=''+=''-=''*=''/='.
+
+# note: Higher in the table evaluated first
+
+
+print("-----Example 1-----------")
+a = 2 + 5 * 6
+b = (2+3) * 4
+
+print(a)
+print(b)
+
+print("------Example 2--------------")
+
+x = 3 
+y = 7
+z = -2 
+
+result = -x ** y % 4
+print(result)
+
+print("------Example 3------------")
+
+a = 5 
+b = 10
+c = 3
+
+result = a + b > c and b % c == 1 | 0
+print(result)
